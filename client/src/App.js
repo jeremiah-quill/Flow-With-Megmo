ee03c85d893f643c92949f879a2d24adeda24c09
Jeremiah Quill <jcq5010@gmail.com>
2022-01-24 17:01:21 -0500

    fixed left and right transitions based on which way up the url tree you are

--- client/src/App.js
+++ client/src/App.js
@@ -5,75 +5,64 @@ import Classes from "./components/Classes.js";
 import Music from "./components/Music.js";
 import Bookings from "./components/Bookings.js";
 import Class from "./components/Class";
-import { Routes, Route, Link, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import SpotifyPlayer from "./components/SpotifyPlayer";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Dashboard from "./components/Dashboard";
 
 function App() {
+
+
 	const location = useLocation();
 
+
+	const [direction, setDirection] = useState(null)
 
 	const getPathDepth = (location) => {
 		let pathArr = location.pathname.split("/");
 		return pathArr.filter((el) => el !== "").length;
 	};
-
-	const [currentPath, setCurrentPath] = useState(location.pathname);
-
-
-
-	const [currentDepth, setCurrentDepth] = useState(getPathDepth(location));
-
-	const [direction, setDirection] = useState("");
-
-	const prevDepthRef = useRef();
-
-	let directionRef = useRef(null)
+		const [locationState, setLocationState] = useState(getPathDepth(location))
 
 
 	useEffect(() => {
-		prevDepthRef.current = currentDepth;
-		setCurrentPath(location.pathname)
-		if (getPathDepth(location) > prevDepthRef.current) {
-			setDirection("right");
-			console.log('right')
+		location.state = locationState
+
+		setLocationState(getPathDepth(location));
+		// setLocationState(locationState += 1)
+		console.log(`old location: ${location.state} newlocation: ${getPathDepth(location)}`)
+		if(getPathDepth(location) - location.state >=0) {
+			console.log('right!')
+			setDirection('right')
 		} else {
-			setDirection("left");
-			console.log('left')
+			console.log('left!')
+			setDirection('left')
 		}
-		console.log(currentPath)
-	});
+
+	}, [location]);
 
 
-const dothething = (location) => {
-	setCurrentDepth(getPathDepth(location));
-	setCurrentPath(location.pathname)
-}
 
 	return (
-		<div>
+		<div className={direction}>
 			<TransitionGroup component={null}>
-				<CSSTransition key={location.key} classNames="fade" timeout={500}>
-					<div
-						className={direction}
-						onClick={() => dothething(location) }
-						
-						ref={directionRef}
-					>
-						<Routes location={location}>
-							<Route exact path="/" element={<Navbar />} />
-							<Route exact path="/classes" element={<Classes />} />
-							<Route exact path="/music" element={<Music />} />
-							<Route exact path="/bookings" element={<Bookings />} />
-							<Route exact path="/classes/:id" element={<Class />} />
-							<Route exact path="/playlists/:id" element={<SpotifyPlayer />} />
-							<Route exact path="/dashboard" element={<Dashboard />} />
+				<CSSTransition
+					key={location.key}
+					classNames={'slide'}
+					timeout={500}
+				>
+					<Routes location={location}>
+						<Route path="/" element={<Navbar />} />
+						<Route path="/classes" element={<Classes />} />
+						<Route path="/music" element={<Music />} />
+						<Route path="/bookings" element={<Bookings />} />
+						<Route path="/classes/:id" element={<Class />} />
+						<Route path="/playlists/:id" element={<SpotifyPlayer />} />
+						<Route path="/dashboard" element={<Dashboard />} />
 
-							{/* add below 404 page */}
-							{/* <Route path="*" element={<NoMatch />} /> */}
-						</Routes>
-					</div>
+						{/* add below 404 page */}
+						{/* <Route path="*" element={<NoMatch />} /> */}
+					</Routes>
 				</CSSTransition>
 			</TransitionGroup>
 		</div>
