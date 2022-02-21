885128a5f21b88164f8407f44b48a2edfd05f96d
Jeremiah Quill <jcq5010@gmail.com>
2022-02-21 13:24:33 -0500

    new layout almost complete

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,51 @@
 import React from "react";
 import Auth from "../utils/auth";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 import "../styles/Navbar.css";
 import { useModalContext } from "../utils/contexts/ModalContext";
+import { useUserContext } from "../utils/contexts/UserContext";
+import UserButtons from "./UserButtons";
 
 function Navbar() {
-	const {resetModal} = useModalContext()
+	const { currentUser } = useUserContext();
 	return (
-		<div className="navbar">
-			<Link onClick={resetModal} className="nav-item" to="/">Home</Link>
-			<Link className="nav-item" to="/classes">Schedule</Link>
-			<Link className="nav-item" to="/contact">Contact Me</Link>
-		</div>
+		<nav className="list-nav">
+			<UserButtons />
+			<Link className="nav-item main-nav-item" to="/">
+				Home
+			</Link>
+			<Link className="nav-item main-nav-item" to="/my-story">
+				My Story
+			</Link>
+			<Link className="nav-item main-nav-item" to="/my-class">
+				My Class
+			</Link>
+			<Link className="nav-item main-nav-item" to="/how-it-works">
+				How It Works
+			</Link>
+			<Link className="nav-item main-nav-item" to="/book-private">
+				Book Private
+			</Link>
+
+			{currentUser.isAdmin ? (
+				<Link className="nav-item" to="/admin-dashboard">
+					Admin Dashboard
+				</Link>
+			) : (
+				""
+			)}
+
+			{currentUser.loggedIn ? (
+				<Link className="nav-item" to="/manage-classes">
+					Manage Classes
+				</Link>
+			) : (
+				""
+			)}
+
+			{/* </ul> */}
+		</nav>
 	);
 }
 
--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -22,14 +22,14 @@ class AuthService {
   login(idToken) {
     // Saves user token to localStorage and reloads the application for logged in status to take effect
     localStorage.setItem('id_token', idToken);
-    window.location.reload();
+    window.location.replace('/manage-classes');
   }
 
   logout() {
     // Clear user token and profile data from localStorage
     localStorage.removeItem('id_token');
     // this will reload the page and reset the state of the application
-    window.location.reload();
+    window.location.replace('/')
   }
 }
 
