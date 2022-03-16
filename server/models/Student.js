8dc70ff7c7e7465e997beee8e73bf19ad14b4938
Jeremiah Quill <jcq5010@gmail.com>
2022-03-16 00:21:57 -0400

    fixed last commit of removing hashing from pws

--- server/models/Student.js
+++ server/models/Student.js
@@ -1,19 +1,18 @@
 const { Schema, model } = require("mongoose");
-const bcrypt = require('bcrypt');
-
+const bcrypt = require("bcrypt");
 
 const studentSchema = new Schema({
 	isAdmin: {
 		type: Boolean,
-		default: false
+		default: false,
 	},
 	username: {
 		type: String,
 		// required: true,
 		trim: true,
-		required: [true, 'Please enter a username.'],
+		required: [true, "Please enter a username."],
 		minlength: 3,
-		maxlength: 30
+		maxlength: 30,
 	},
 	registeredClasses: [
 		{
@@ -24,13 +23,15 @@ const studentSchema = new Schema({
 	email: {
 		type: String,
 		lowercase: true,
-		required: [true, 'email is required'],
-		match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Invalid email format'],
+		required: [true, "email is required"],
+		match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, "Invalid email format"],
 		validate: {
-			validator: function(v){
-				return this.model('Student').findOne({ email: v }).then(user => !user)
+			validator: function (v) {
+				return this.model("Student")
+					.findOne({ email: v })
+					.then((user) => !user);
 			},
-			message: props => `Email is already used by another user.`
+			message: (props) => `Email is already used by another user.`,
 		},
 	},
 	password: {
@@ -38,37 +39,34 @@ const studentSchema = new Schema({
 		required: true,
 		trim: true,
 		minlength: 3,
-		maxlength: 30
+		maxlength: 30,
 	},
 	isSendNotifications: {
 		type: Boolean,
 		default: true,
-	}
+	},
 });
 
 // set up pre-save middleware to create password
-// studentSchema.pre('save', async function (next) {
-// 	if (this.isNew || this.isModified('password')) {
-// 	  const saltRounds = 10;
-// 	  this.password = await bcrypt.hash(this.password, saltRounds);
-// 	}
-  
-// 	next();
-//   });
+studentSchema.pre('save', async function (next) {
+	if (this.isNew || this.isModified('password')) {
+	  const saltRounds = 10;
+	  this.password = await bcrypt.hash(this.password, saltRounds);
+	}
+
+	next();
+  });
 
 // compare the incoming password with the hashed password
 studentSchema.methods.isCorrectPassword = async function (password) {
 	// return password === this.password
 
-	if(this.isAdmin === true) {
-		return password === this.password
-
-	} else {
-		return password === this.password
-
-	// return bcrypt.compare(password, this.password);
+	if (this.isAdmin === true) {
+		return password === this.password;
 	}
-  };
+
+	return bcrypt.compare(password, this.password);
+};
 
 const Student = model("Student", studentSchema);
 
